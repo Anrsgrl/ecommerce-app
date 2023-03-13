32823f0415a4a8c6200c0faa7726f522a8252450
Anrsgrl <esgerlianar003@gmail.com>
2023-03-13 11:44:48 +0300

    cartSlice added

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cartReducer } from "./features/cartSlice";
 import { productReducer } from "./features/productsSlice";
 
 export const store = configureStore({
     reducer: {
         products: productReducer,
+        cart: cartReducer,
     },
 });
