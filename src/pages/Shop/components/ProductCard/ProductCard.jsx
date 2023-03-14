0dc07300a047920e336725c4097fb2e00073cbcd
Anrsgrl <esgerlianar003@gmail.com>
2023-03-14 18:58:40 +0300

    animation added to fav button

--- src/pages/Shop/components/ProductCard/ProductCard.jsx
+++ src/pages/Shop/components/ProductCard/ProductCard.jsx
@@ -1,21 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./productCard.scss";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { MdFavorite } from "react-icons/md";
 import { useDispatch } from 'react-redux';
 import { addToCart } from '../../../../store/features/cartSlice';
+import { addToFav } from '../../../../store/features/favoriteSlice';
 
 const ProductCard = (props) => {
     const { image, title, category, price, keey, product } = props;
     const dispatch = useDispatch()
-
+    const [fav, setFav] = useState(false);
     return (
         <div key={keey} className="col-6 col-lg-4 pb-5 product-card d-flex flex-column">
             <div className="product-image">
                 <img src={image} alt="productImg" />
                 <div className="product-icons">
                     <button onClick={() => dispatch(addToCart(product))}><AiOutlineShoppingCart /></button>
-                    <button className='like'><MdFavorite /></button>
+                    <button onClick={() => { dispatch(addToFav(product)); setFav(!fav) }} className={fav ? "fav-active" : ""}><MdFavorite /></button>
                 </div>
             </div>
             <div className="product-details d-flex flex-column">
@@ -23,7 +24,7 @@ const ProductCard = (props) => {
                 <h5>{title}</h5>
                 <span className='product-price'>${Math.round(price)}.00</span>
             </div>
-        </div>
+        </div >
     )
 }
 
